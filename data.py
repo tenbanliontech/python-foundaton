c1fc4b713fce5be48c3420e8d0bcb398ba11620e
“prof <temban@liontechacademy.com>
2024-06-09 23:54:57 +0100

    my first python foundation

---
+++ data.py
@@ -0,0 +1,24 @@
+name = "jack"
+print(name)
+
+car = "hyundai"
+print(type(car))
+
+x = 12
+y = 10
+z = x + y
+print(z)
+
+print(type(z))
+
+x = 3
+y = 2
+z = x // y
+print(z)
+
+print(type(z))
+
+name = "joel"
+print(name)
+
+print(type(name))
